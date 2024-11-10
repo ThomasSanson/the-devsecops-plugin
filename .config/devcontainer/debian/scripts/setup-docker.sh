e00123e68f99da56d777b2d3af2bf9ef936f1d74
Thomas Sanson <thomas.sanson@education.gouv.fr>
2024-11-10 15:43:42 +0000

    chore(setup-docker.sh): remove Docker group ID modification to simplify setup process and avoid potential issues with existing group IDs

--- .config/devcontainer/debian/scripts/setup-docker.sh
+++ .config/devcontainer/debian/scripts/setup-docker.sh
@@ -9,10 +9,8 @@ source "$TEMP_DIR/env_vars"
 
 setup_docker() {
   local USERNAME="$1"
-  local DOCKER_GROUP_ID="$2"
 
   echo "Configuring Docker..."
-  sudo groupmod -g "$DOCKER_GROUP_ID" docker
   sudo usermod -aG docker "$USERNAME"
 
   echo "Docker configuration completed."
