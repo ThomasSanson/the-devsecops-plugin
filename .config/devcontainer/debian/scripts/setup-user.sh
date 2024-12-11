046ff694d3a0332e7216c75c6b2b7d0e0bc1eb8a
Thomas Sanson <thomas.sanson@education.gouv.fr>
2024-12-11 16:29:17 +0000

    refactor(setup-user.sh): rename USERNAME variable to USER for consistency and clarity in user setup script

--- .config/devcontainer/debian/scripts/setup-user.sh
+++ .config/devcontainer/debian/scripts/setup-user.sh
@@ -8,21 +8,21 @@ set -e
 source "$TEMP_DIR/env_vars"
 
 setup_user() {
-  local USERNAME="$1"
+  local USER="$1"
 
-  echo "Setting up non-root user: $USERNAME"
-  useradd -m "$USERNAME"
-  echo "$USERNAME ALL=(root) NOPASSWD:ALL" >"/etc/sudoers.d/$USERNAME"
-  chmod 0440 "/etc/sudoers.d/$USERNAME"
+  echo "Setting up non-root user: $USER"
+  useradd -m "$USER"
+  echo "$USER ALL=(root) NOPASSWD:ALL" >"/etc/sudoers.d/$USER"
+  chmod 0440 "/etc/sudoers.d/$USER"
 
-  echo "Configuring shell for $USERNAME"
-  echo 'source ~/.bashrc' >>"/home/$USERNAME/.bash_profile"
-  echo "alias ll='ls -lisa --color'" >>"/home/$USERNAME/.bashrc"
+  echo "Configuring shell for $USER"
+  echo 'source ~/.bashrc' >>"/home/$USER/.bash_profile"
+  echo "alias ll='ls -lisa --color'" >>"/home/$USER/.bashrc"
 
   # Setup Taskfile completion
-  wget --progress=dot:giga https://raw.githubusercontent.com/go-task/task/main/completion/bash/task.bash -O "/home/$USERNAME/task.bash"
-  chmod +x "/home/$USERNAME/task.bash"
-  echo 'source ~/task.bash' >>"/home/$USERNAME/.bashrc"
+  wget --progress=dot:giga https://raw.githubusercontent.com/go-task/task/main/completion/bash/task.bash -O "/home/$USER/task.bash"
+  chmod +x "/home/$USER/task.bash"
+  echo 'source ~/task.bash' >>"/home/$USER/.bashrc"
 
   echo "Non-root user setup completed."
 }
