b0fc3b85994ef5f7956767ddab3190b7ec03df74
Thomas Sanson <thomas.sanson@education.gouv.fr>
2025-06-18 12:51:39 +0200

    feat(devcontainer): add script to install necessary packages for development environment setup

---
+++ .config/devcontainer/ubuntu/scripts/install-packages.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# shellcheck disable=SC1091
+source "$TEMP_DIR/env_vars"
+
+install_packages() {
+  echo "Updating package lists..."
+  apt-get update
+
+  echo "Installing necessary packages..."
+  apt-get install -y --no-install-recommends \
+    bash-completion \
+    curl \
+    git \
+    gnupg2 \
+    lsb-release \
+    nano \
+    openssh-client \
+    python3 \
+    "python3.12" \
+    "python3.12-venv" \
+    "python3.12-dev" \
+    sudo \
+    unzip \
+    wget
+
+  echo "Package installation completed."
+}
+
+install_packages
