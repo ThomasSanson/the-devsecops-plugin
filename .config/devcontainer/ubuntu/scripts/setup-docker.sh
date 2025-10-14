57525a2ad1d246eb20b05b596ab49af86b80127c
Thomas Sanson <thomas.sanson@education.gouv.fr>
2025-10-15 00:37:35 +0200

    feat(setup-docker.sh): add check and creation of docker group if it doesn't exist to enhance setup process

--- .config/devcontainer/ubuntu/scripts/setup-docker.sh
+++ .config/devcontainer/ubuntu/scripts/setup-docker.sh
@@ -11,6 +11,13 @@ setup_docker() {
   local USER="$1"
 
   echo "Configuring Docker..."
+
+  # Create docker group if it doesn't exist
+  if ! getent group docker >/dev/null 2>&1; then
+    echo "Creating docker group..."
+    sudo groupadd docker
+  fi
+
   sudo usermod -aG docker "$USER"
 
   echo "Docker configuration completed."
